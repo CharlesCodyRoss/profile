33e20ce1f400b404c55101d85c10eba104dd31c9
Charles Ross <charles.cody.ross@gmail.com>
2022-09-20 18:34:05 +0100

    Update Streamlit

--- profile_app.py
+++ profile_app.py
@@ -2,9 +2,19 @@ import streamlit as st
 
 st.title("Begin Profile Page")
 
+
+
 intro, overview, pro_work, per_work, contact  = st.tabs([
                             "Introduction", 
                             "Overview of Experience and Skills", 
                             "Professional Work", 
                             "Personal Projects",
                             "Contact Information"])
+
+
+with st.expander("See explanation", expanded = True):
+     st.write("""
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    """)
